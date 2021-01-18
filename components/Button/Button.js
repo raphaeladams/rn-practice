353a2b296f0d7a0edcf6d160fe463153b642c98b
Raphael Adams <raphael.adams@shopify.com>
2021-01-18 16:17:25 -0500

    Completed Expo Getting Started tutorial

---
+++ components/Button/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+
+export default function Button(props) {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={styles.button}
+    >
+      <Text style={styles.buttonText}>
+        {props.btnText}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 15,
+    borderRadius: 5,
+    margin: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#000'
+  }
+});
